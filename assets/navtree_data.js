c16e232b8626975f1cbef8c863fa7e6d6ade3589
skwok <stephen.kwok@magnet.com>
2014-09-15 09:59:36 -0700

    Initial commit of Javadoc

---
+++ assets/navtree_data.js
@@ -0,0 +1,11 @@
+var NAVTREE_DATA =
+[ [ "com.magnet.android.mms", "com/magnet/android/mms/package-summary.html", [ [ "Classes", null, [ [ "MagnetMobileClient", "com/magnet/android/mms/MagnetMobileClient.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.magnet.android.mms.async", "com/magnet/android/mms/async/package-summary.html", [ [ "Interfaces", null, [ [ "Call", "com/magnet/android/mms/async/Call.html", null, "" ], [ "StateChangedListener", "com/magnet/android/mms/async/StateChangedListener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "StateChangedListener.ProgressData", "com/magnet/android/mms/async/StateChangedListener.ProgressData.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "Call.State", "com/magnet/android/mms/async/Call.State.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ reference/lists.js
@@ -0,0 +1,10 @@
+var DATA = [
+      { id:0, label:"com.magnet.android.mms", link:"com/magnet/android/mms/package-summary.html", type:"package" },
+      { id:1, label:"com.magnet.android.mms.MagnetMobileClient", link:"com/magnet/android/mms/MagnetMobileClient.html", type:"class" },
+      { id:2, label:"com.magnet.android.mms.async", link:"com/magnet/android/mms/async/package-summary.html", type:"package" },
+      { id:3, label:"com.magnet.android.mms.async.Call", link:"com/magnet/android/mms/async/Call.html", type:"class" },
+      { id:4, label:"com.magnet.android.mms.async.Call.State", link:"com/magnet/android/mms/async/Call.State.html", type:"class" },
+      { id:5, label:"com.magnet.android.mms.async.StateChangedListener", link:"com/magnet/android/mms/async/StateChangedListener.html", type:"class" },
+      { id:6, label:"com.magnet.android.mms.async.StateChangedListener.ProgressData", link:"com/magnet/android/mms/async/StateChangedListener.ProgressData.html", type:"class" }
+
+    ];
