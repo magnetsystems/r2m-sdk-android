9610e61362a5672bf9a5b0a1a0eae98cea8f5ad2
skwok <stephen.kwok@magnet.com>
2014-10-03 11:07:04 -0700

    javadoc for sdk version 1.1.0

--- assets/navtree_data.js
+++ assets/navtree_data.js
@@ -2,8 +2,11 @@ var NAVTREE_DATA =
 [ [ "com.magnet.android.mms", "com/magnet/android/mms/package-summary.html", [ [ "Classes", null, [ [ "MagnetMobileClient", "com/magnet/android/mms/MagnetMobileClient.html", null, "" ] ]
 , "" ] ]
 , "" ], [ "com.magnet.android.mms.async", "com/magnet/android/mms/async/package-summary.html", [ [ "Interfaces", null, [ [ "Call", "com/magnet/android/mms/async/Call.html", null, "" ], [ "StateChangedListener", "com/magnet/android/mms/async/StateChangedListener.html", null, "" ] ]
-, "" ], [ "Classes", null, [ [ "StateChangedListener.ProgressData", "com/magnet/android/mms/async/StateChangedListener.ProgressData.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "CallImpl", "com/magnet/android/mms/async/CallImpl.html", null, "" ], [ "StateChangedListener.ProgressData", "com/magnet/android/mms/async/StateChangedListener.ProgressData.html", null, "" ] ]
 , "" ], [ "Enums", null, [ [ "Call.State", "com/magnet/android/mms/async/Call.State.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.magnet.android.mms.exception", "com/magnet/android/mms/exception/package-summary.html", [ [ "Classes", null, [ [ "MarshallingException", "com/magnet/android/mms/exception/MarshallingException.html", null, "" ], [ "SchemaException", "com/magnet/android/mms/exception/SchemaException.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "HttpCallException", "com/magnet/android/mms/exception/HttpCallException.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
--- reference/lists.js
+++ reference/lists.js
@@ -4,7 +4,12 @@ var DATA = [
       { id:2, label:"com.magnet.android.mms.async", link:"com/magnet/android/mms/async/package-summary.html", type:"package" },
       { id:3, label:"com.magnet.android.mms.async.Call", link:"com/magnet/android/mms/async/Call.html", type:"class" },
       { id:4, label:"com.magnet.android.mms.async.Call.State", link:"com/magnet/android/mms/async/Call.State.html", type:"class" },
-      { id:5, label:"com.magnet.android.mms.async.StateChangedListener", link:"com/magnet/android/mms/async/StateChangedListener.html", type:"class" },
-      { id:6, label:"com.magnet.android.mms.async.StateChangedListener.ProgressData", link:"com/magnet/android/mms/async/StateChangedListener.ProgressData.html", type:"class" }
+      { id:5, label:"com.magnet.android.mms.async.CallImpl", link:"com/magnet/android/mms/async/CallImpl.html", type:"class" },
+      { id:6, label:"com.magnet.android.mms.async.StateChangedListener", link:"com/magnet/android/mms/async/StateChangedListener.html", type:"class" },
+      { id:7, label:"com.magnet.android.mms.async.StateChangedListener.ProgressData", link:"com/magnet/android/mms/async/StateChangedListener.ProgressData.html", type:"class" },
+      { id:8, label:"com.magnet.android.mms.exception", link:"com/magnet/android/mms/exception/package-summary.html", type:"package" },
+      { id:9, label:"com.magnet.android.mms.exception.HttpCallException", link:"com/magnet/android/mms/exception/HttpCallException.html", type:"class" },
+      { id:10, label:"com.magnet.android.mms.exception.MarshallingException", link:"com/magnet/android/mms/exception/MarshallingException.html", type:"class" },
+      { id:11, label:"com.magnet.android.mms.exception.SchemaException", link:"com/magnet/android/mms/exception/SchemaException.html", type:"class" }
 
     ];
